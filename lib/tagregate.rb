2040ac2ea6bcd11521d28462468d5ff6cb6d6c21
Bryan Woods & Matt Vermaak <pair+bryan+matt@howaboutwe.com>
2012-03-16 16:57:14 -0400

    Adding the initial codez; not working

---
+++ lib/tagregate.rb
@@ -0,0 +1,19 @@
+require 'cucumber/rake/task'
+Dir["/tagregate/*.rb"].each { |file| require file }
+
+def load_rake_tasks
+  Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each do |f|
+    puts "Loading #{f}"
+    load f
+  end
+end
+
+if defined?(Rails)
+  class TagregateTasks < Rails::Railtie
+    rake_tasks do
+      load_rake_tasks
+    end
+  end
+else
+  raise "Sorry, tagregate currently only works with Ruby on Rails applications."
+end
